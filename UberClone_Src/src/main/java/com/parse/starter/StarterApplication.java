35119b205c521ad7a152de3c6d9bf2b479c9acc2
thinhman <thinhnguyen.nguyen2353@gmail.com>
2019-05-22 17:33:48 -0700

    Add files via upload
    Whole project was too big to upload, this is just the source code.

---
+++ UberClone_Src/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("b2cc8094ba3b7a55c58cbabd9781b4cfbc52b8af")
+            .clientKey("45d3d7b33e1056f4d9da5e9321eff0311f86d7b0")
+            .server("http://18.219.26.92:80/parse/")
+            .build()
+    );
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
